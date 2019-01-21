b816a1883c477b6a20aa326d2e92a0295806b6b6
Hazrat Ali <iloveyii@yahoo.com>
2019-01-22 00:18:32 +0100

    Added Calculate class

---
+++ src/js/Calculate.js
@@ -0,0 +1,35 @@
+import {PRODUCT_TYPE_PRICES, REBATE, USER_TYPE} from "./config/settings";
+import moment from 'moment';
+
+
+/**
+ * This is a Product object which contains three properties
+ * @param number productType - new 1 , old - 2
+ * @param number price - the price of the product
+ * @param date publishedDate - the publish date of the product
+ * @constructor
+ */
+export default class Calculate {
+    constructor(product, user) {
+        this.product = product;
+        this.user = user;
+    }
+
+    price() {
+        return this.product.price + PRODUCT_TYPE_PRICES[this.product.productType] - this.rebate();
+    }
+
+    rebate() {
+        // If the product is new and published today - rebate of 10 sek
+        if(this.product.publishedToday() && this.product.isNew()) {
+            return REBATE.PRODUCT_IS_NEW_AND_PUBLISHED_TODAY;
+        }
+
+        // If you user is a company user - rebate 5
+        if(this.user.userType === USER_TYPE.COMPANY_USER) {
+            return REBATE.COMPANY_USER;
+        }
+
+        return REBATE.DEFAULT;
+    }
+}
--- src/js/config/settings.js
+++ src/js/config/settings.js
@@ -7,3 +7,14 @@ export const PRODUCT_TYPE = {
     NEW : 1,
     OLD: 2
 };
+
+export const PRODUCT_TYPE_PRICES = {
+    NEW : 25,
+    OLD: 35
+};
+
+export const REBATE = {
+    PRODUCT_IS_NEW_AND_PUBLISHED_TODAY : 10,
+    COMPANY_USER : 5,
+    DEFAULT : 0
+};
